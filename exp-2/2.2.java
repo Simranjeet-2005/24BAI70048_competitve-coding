41468f73e73975a7906c04a9b0d1e3d80ec58151
unknown <simranjeet3059@gmail.com>
2026-07-28 11:45:02 +0530

    Updated Java code

--- exp-2/2.2.java
+++ exp-2/2.2.java
@@ -1,29 +1,29 @@
-import java.util.*;
-public class mutliple_1{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter n");
-        int n=sc.nextInt();
-        int res[]=new int[n];
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        int prefix[]=new int[n];
-        int suffix[]=new int[n];
-        prefix[0]=1;
-        suffix[n-1]=1;
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]*arr[i-1];
-        }
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]*arr[i+1];
-        }
-        for(int i=0;i<n;i++){
-            res[i]=prefix[i]*suffix[i];
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(res[i]+" ");
+    import java.util.*;
+    public class mutliple_1{
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter n");
+            int n=sc.nextInt();
+            int res[]=new int[n];
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            int prefix[]=new int[n];
+            int suffix[]=new int[n];
+            prefix[0]=1;
+            suffix[n-1]=1;
+            for(int i=1;i<n;i++){
+                prefix[i]=prefix[i-1]*arr[i-1];
+            }
+            for(int i=n-2;i>=0;i--){
+                suffix[i]=suffix[i+1]*arr[i+1];
+            }
+            for(int i=0;i<n;i++){
+                res[i]=prefix[i]*suffix[i];
+            }
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
         }
     }
-}
